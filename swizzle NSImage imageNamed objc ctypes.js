6a0f19690c62c864ea1bbeddfd152845ba329179
Noitidart <noitidart@gmail.com>
2015-01-30 23:21:01 -0800

    rev156
    ok created my class, untested though

--- swizzle NSImage imageNamed objc ctypes.js
+++ swizzle NSImage imageNamed objc ctypes.js
@@ -7,11 +7,13 @@ var jsStr_imagePath = OS.Path.join(OS.Constants.Path.desktopDir, 'beta.icns');
 /** END - edit these **/
 
 // types
+var CHAR = ctypes.char;
 var CLASS = ctypes.voidptr_t;
 var ID = ctypes.voidptr_t;
 var SEL = ctypes.voidptr_t;
 var BOOL = ctypes.signed_char;
-var NSUInteger = ctypes.unsigned_long;
+var NSUINTEGER = ctypes.unsigned_long;
+var SIZE_T = ctypes.size_t;
 var VOID = ctypes.void_t;
 
 // advanced types
@@ -28,24 +30,45 @@ var METHOD = objc_method.ptr;
 var NIL = ctypes.voidptr_t(0);
 
 //common functions
-var objc_getClass = objc.declare('objc_getClass', ctypes.default_abi, ID, ctypes.char.ptr);
-var sel_registerName = objc.declare('sel_registerName', ctypes.default_abi, SEL, ctypes.char.ptr);
+var objc_getClass = objc.declare('objc_getClass', ctypes.default_abi, ID, CHAR.ptr);
+var sel_registerName = objc.declare('sel_registerName', ctypes.default_abi, SEL, CHAR.ptr);
 var objc_msgSend = objc.declare('objc_msgSend', ctypes.default_abi, ID, ID, SEL, '...');
 var method_setImplementation = objc.declare('method_setImplementation', ctypes.default_abi, IMP, METHOD, IMP);
 var class_getInstanceMethod = objc.declare('class_getInstanceMethod', ctypes.default_abi, METHOD, CLASS, SEL);
 var method_getImplementation = objc.declare('method_getImplementation', ctypes.default_abi, IMP, METHOD);
 var method_exchangeImplementations = objc.declare('method_exchangeImplementations', ctypes.default_abi, VOID, METHOD, METHOD);
 var release = sel_registerName('release');
+var objc_disposeClassPair = objc.declare('objc_disposeClassPair', ctypes.default_abi, VOID, CLASS);
+var objc_allocateClassPair = objc.declare('objc_allocateClassPair', ctypes.default_abi, CLASS, CLASS, CHAR.ptr, SIZE_T);
+var class_addMethod = objc.declare('class_addMethod', ctypes.default_abi, BOOL, CLASS, SEL, IMP, CHAR.ptr);
 
-var shutdownFunc;
+// my globals
 var myIcon;
+var instance__class_NoitSwizzler;
+var class_NoitSwizzler;
+
+function shutdown() {
+	//put code here to unswizzle it
+	if (myIcon) {
+		objc_msgSend(myIcon, release);
+	}
+	
+	if (instance__class_NoitSwizzler) {
+		objc_msgSend(instance__class_NoitSwizzler, release);
+	}
+	
+	if (class_NoitSwizzler) {
+		objc_disposeClassPair(class_NoitSwizzler);
+	}
+	objc.close();
+};
 
 var promise_makeMyNSImage = OS.File.read(jsStr_imagePath);
 
 promise_makeMyNSImage.then(
 	function(iconData) {
 		// NOTE: iconData is Uint8Array
-		var length = NSUInteger(iconData.length);
+		var length = NSUINTEGER(iconData.length);
 		var bytes = ctypes.uint8_t.array()(iconData);
 
 		// data = [NSData dataWithBytes: bytes length: length];
@@ -64,20 +87,23 @@ promise_makeMyNSImage.then(
 		
 		var NSImage = objc_getClass('NSImage');
 		var imageNamed = sel_registerName('imageNamed');
-		var UTF8String = _sel_registerName('UTF8String');
+		var UTF8String = sel_registerName('UTF8String');
 		
+		var classs = sel_registerName('class');
 		var js_swizzled_imageNamed = function(c_arg1__self, c_arg2__sel, objc_arg1__NSStringPtr) {
 			console.log('SWIZZLED: imageNamed called');
 
 			var tt_read = objc_msgSend(objc_arg1__NSStringPtr, UTF8String);
 			console.info('tt_read:', tt_read, tt_read.toString(), uneval(tt_read), tt_read.isNull());
-			var tt_read_casted = ctypes.cast(tt_read, ctypes.char.ptr);
+			var tt_read_casted = ctypes.cast(tt_read, CHAR.ptr);
 			console.info('tt_read_casted:', tt_read_casted, tt_read_casted.toString(), uneval(tt_read_casted), tt_read_casted.isNull());
 			var tt_read_jsStr = tt_read_casted.readStringReplaceMalformed();
 			console.info('tt_read_jsStr:', tt_read_jsStr, tt_read_jsStr.toString(), uneval(tt_read_jsStr)); // TypeError: tt_read_jsStr.isNull is not a function 
 			if (tt_read_jsStr == 'NSApplication') {
+				// do my hook
 				return myIcon;
 			} else {
+				// do normal
 				var icon = objc_msgSend(NSImage, imageNamed, objc_arg1__NSStringPtr);
 				return icon;
 			}
@@ -85,27 +111,39 @@ promise_makeMyNSImage.then(
 
 		var swizzled_imageNamed = IMP.ptr(js_swizzled_imageNamed);
 		
-		var originalMethod = class_getInstanceMethod(NSImage, imageNamed);
-		console.info('originalMethod:', originalMethod, originalMethod.toString(), uneval(originalMethod));
-		// var alternateMethod = class_getInstanceMethod(alternateClass, alternateSelector);
-		// console.info('alternateMethod:', alternateMethod, alternateMethod.toString(), uneval(alternateMethod));
-		// var imp_of_alternateMethod = method_getImplementation(alternateMethod)
-		// console.info('imp_of_alternateMethod:', imp_of_alternateMethod, imp_of_alternateMethod.toString(), uneval(imp_of_alternateMethod));
+		//start make my class
+		var NSObject = objc_getClass('NSObject'); // because NSImage is child of NSObject ill do same for my class
+		class_NoitSwizzler = objc_allocateClassPair(NSObject, 'NoitSwizzler', 0);
 		
-		// var rez = method_setImplementation(originalMethod, imp_of_alternateMethod);
-		// console.info('rez:', rez, rez.toString(), uneval(rez));
+		console.info('class_NoitSwizzler:', class_NoitSwizzler, class_NoitSwizzler.toString(), uneval(class_NoitSwizzler), class_NoitSwizzler.isNull());
+		if (class_NoitSwizzler.isNull()) {
+			console.info('class_NoitSwizzler:', class_NoitSwizzler, class_NoitSwizzler.toString(), uneval(class_NoitSwizzler));
+			throw new Error('class_NoitSwizzler is NIL, so objc_allocateClassPair failed');
+		}
+		
+		var rez_class_addMethod = class_addMethod(class_NoitSwizzler, imageNamed, swizzled_imageNamed, '@@:@'); // because return of callback is NSImage so `ctypes.voidptr_t`, first argument is c_arg1__self which is `id` and c_arg2__id sel `SEL` and objc_arg1__NSStringPtr is `voidptr_t`
+		console.info('rez_class_addMethod:', rez_class_addMethod, rez_class_addMethod.toString(), uneval(rez_class_addMethod));
+		if (rez_class_addMethod != 1) {
+			throw new Error('rez_class_addMethod is not 1, so class_addMethod failed');
+		}
+		
+		objc_registerClassPair(class_NoitSwizzler); // return is void
+		
+		instance__class_NoitSwizzler = objc_msgSend(objc_msgSend(class_NoitidartsOnScreenSaverStartedDelegateClass, alloc), init);
+		console.info('instance__class_NoitSwizzler:', instance__class_NoitSwizzler, instance__class_NoitSwizzler.toString(), uneval(instance__class_NoitSwizzler), instance__class_NoitSwizzler.isNull());
+		//end make my class
+		
+		var NSImageClass = objc_msgSend(NSImage, classs);
+		
+		var originalMethod = class_getInstanceMethod(NSImage, imageNamed);  //may need to use `NSImageClass` instead of `NSImage`
+		console.info('originalMethod:', originalMethod, originalMethod.toString(), uneval(originalMethod));
+		
+		
 		var alternateMethod = class_getInstanceMethod(NSImage, imageNamed);
 		console.info('alternateMethod:', alternateMethod, alternateMethod.toString(), uneval(alternateMethod));
 		
 		var rez = method_exchangeImplementations(originalMethod, alternateMethod);
 		// rez is void
-		
-		shutdownFunc = function() {
-			//put code here to unswizzle it
-			objc_msgSend(myIcon, release);
-			objc.close();
-		};
-
 		console.log('SUCCESFULLY SWIZZLED');
 	},
 	function(aReason) {
@@ -114,9 +152,6 @@ promise_makeMyNSImage.then(
 	  throw rejObj;
 	}
 ).catch(function(aCaught) {
-	if (myIcon) {
-		objc_msgSend(myIcon, release);
-	}
-	objc.close();
+	shutdown();
 	console.error('promise_makeMyNSImage Catch:', aCaught);
 });

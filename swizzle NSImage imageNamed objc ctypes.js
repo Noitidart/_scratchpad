e47f77b1421d5fb81276bb571afb20e3881c8df8
Noitidart <noitidart@gmail.com>
2015-01-30 22:47:35 -0800

    rev155
    added in the excahngeImplementations stuff, but i need to create my
    alternateMethod im having tricky time with that

--- swizzle NSImage imageNamed objc ctypes.js
+++ swizzle NSImage imageNamed objc ctypes.js
@@ -7,10 +7,12 @@ var jsStr_imagePath = OS.Path.join(OS.Constants.Path.desktopDir, 'beta.icns');
 /** END - edit these **/
 
 // types
+var CLASS = ctypes.voidptr_t;
 var ID = ctypes.voidptr_t;
 var SEL = ctypes.voidptr_t;
 var BOOL = ctypes.signed_char;
 var NSUInteger = ctypes.unsigned_long;
+var VOID = ctypes.void_t;
 
 // advanced types
 var IMP = ctypes.FunctionType(ctypes.default_abi, ID, [ID, SEL, '...']).ptr;
@@ -30,7 +32,9 @@ var objc_getClass = objc.declare('objc_getClass', ctypes.default_abi, ID, ctypes
 var sel_registerName = objc.declare('sel_registerName', ctypes.default_abi, SEL, ctypes.char.ptr);
 var objc_msgSend = objc.declare('objc_msgSend', ctypes.default_abi, ID, ID, SEL, '...');
 var method_setImplementation = objc.declare('method_setImplementation', ctypes.default_abi, IMP, METHOD, IMP);
-
+var class_getInstanceMethod = objc.declare('class_getInstanceMethod', ctypes.default_abi, METHOD, CLASS, SEL);
+var method_getImplementation = objc.declare('method_getImplementation', ctypes.default_abi, IMP, METHOD);
+var method_exchangeImplementations = objc.declare('method_exchangeImplementations', ctypes.default_abi, VOID, METHOD, METHOD);
 var release = sel_registerName('release');
 
 var shutdownFunc;
@@ -81,12 +85,28 @@ promise_makeMyNSImage.then(
 
 		var swizzled_imageNamed = IMP.ptr(js_swizzled_imageNamed);
 		
+		var originalMethod = class_getInstanceMethod(NSImage, imageNamed);
+		console.info('originalMethod:', originalMethod, originalMethod.toString(), uneval(originalMethod));
+		// var alternateMethod = class_getInstanceMethod(alternateClass, alternateSelector);
+		// console.info('alternateMethod:', alternateMethod, alternateMethod.toString(), uneval(alternateMethod));
+		// var imp_of_alternateMethod = method_getImplementation(alternateMethod)
+		// console.info('imp_of_alternateMethod:', imp_of_alternateMethod, imp_of_alternateMethod.toString(), uneval(imp_of_alternateMethod));
+		
+		// var rez = method_setImplementation(originalMethod, imp_of_alternateMethod);
+		// console.info('rez:', rez, rez.toString(), uneval(rez));
+		var alternateMethod = class_getInstanceMethod(NSImage, imageNamed);
+		console.info('alternateMethod:', alternateMethod, alternateMethod.toString(), uneval(alternateMethod));
+		
+		var rez = method_exchangeImplementations(originalMethod, alternateMethod);
+		// rez is void
+		
 		shutdownFunc = function() {
+			//put code here to unswizzle it
 			objc_msgSend(myIcon, release);
 			objc.close();
-		}
+		};
 
-	  
+		console.log('SUCCESFULLY SWIZZLED');
 	},
 	function(aReason) {
 	  var rejObj = {nameOfRejector:'promise_makeMyNSImage', aReason: aReason)};

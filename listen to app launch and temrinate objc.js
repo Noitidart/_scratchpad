69f2f0f7a6379396543377f556fa966384d0dd12
Noitidart <noitidart@gmail.com>
2015-02-02 20:09:21 -0800

    rev171
    works but callback just does a log nothing special

--- listen to app launch and temrinate objc.js
+++ listen to app launch and temrinate objc.js
@@ -1,20 +1,13 @@
-Cu.import('resource://gre/modules/osfile.jsm');
 Cu.import('resource://gre/modules/ctypes.jsm');
-var objc = ctypes.open(ctypes.libraryName('objc'));
 
-/** START - edit these **/
-var jsStr_imagePath = OS.Path.join(OS.Constants.Path.desktopDir, 'ff-logos', 'beta48.png');
-var jsStr_notifName = 'com.apple.screensaver.didstart';
-/** END - edit these **/
+var objc = ctypes.open(ctypes.libraryName('objc'));
 
 // BASIC TYPES
 var BOOL = ctypes.signed_char;
 var CHAR = ctypes.char;
 var CLASS = ctypes.voidptr_t;
 var ID = ctypes.voidptr_t;
-var METHOD = ctypes.voidptr_t;
 var NSNOTIFICATIONPTR = ctypes.voidptr_t;
-var NSUINTEGER = ctypes.unsigned_long;
 var SEL = ctypes.voidptr_t;
 var SIZE_T = ctypes.size_t
 var VOID = ctypes.void_t;
@@ -40,11 +33,8 @@ var init = sel_registerName('init');
 var release = sel_registerName('release');
 
 // my personal globals for this code
-var notif_name;
-var cb_onNotif; // important to make this global otherwise the callback will be GC'ed and firefox will crash
-// my personal globals for this code
+var NoitNotifCB_imp; // important to make this global otherwise on callback trigger firefox will crash, because this got GC'ed before calback triggered
 var releaseThese = [];
-
 var shutdown = function() {
 	for (var i=0; i<releaseThese.length; i++) {
 		objc_msgSend(releaseThese[i], release);
@@ -56,14 +46,44 @@ var shutdown = function() {
 }
 
 function main() {
-	/*
-	// notif_name = [[NSString initWithUTF8String:] jsStr_notifName]' 
+	
+	// the reason i create the class first is because it starts off by running objc_allocateClassPair, if that errors, then that means theres no need to continue and do teh other stuff, that will mess up the unloadings in shutdown() and it wont shutdown properly, this was evident because i ran this code again and it couldn't allocate even after i ran shutdown(). well actually shutdown() didnt shutdown properly on consequent run because i redefined `var shutdown` before running main so duh tahts what broke it. but i would still like to place this first because then i dont unnecessarily allocate an extra NSString etc.
+	// start - create class and callback
+	var NSObject = objc_getClass('NSObject');
+	var NoitNotifCls_class = objc_allocateClassPair(NSObject, 'NoitNotifCls', 0);
+	console.info('NoitNotifCls_class:', NoitNotifCls_class, NoitNotifCls_class.toString(), uneval(NoitNotifCls_class), NoitNotifCls_class.isNull());
+	if (NoitNotifCls_class.isNull()) {
+		throw new Error('NoitNotifCls_class is NIL, so objc_allocateClassPair failed. objc_registerClassPair likely already ran prior to this on NoitNoitfCls_class and objc_disposeOfClassPair was not yet run on NoitNoitfCls_class'); // this happens on consequent runs after `objc_registerClassPair` is run on `NoitNotifCls_class`
+	}
+
+	function NoitNotifCB_js(c_arg1__self, c_arg2__sel, objc_arg1_NSNOTIFICATIONPTR) {
+		console.log('NoitNotifCB_js Triggered');
+		return NIL;
+	}
+	
+	NoitNotifCB_imp = IMP_for_notif_cb(NoitNotifCB_js);
+	
+	var NoitNotifCB_selector = sel_registerName('NoitNoitfCB_objc');
+	var rez_class_addMethod = class_addMethod(NoitNotifCls_class, NoitNotifCB_selector, NoitNotifCB_imp, 'v@:@'); // because return of callback is void, first argument is c_arg1__self which is `id` and c_arg2__id sel `SEL` and objc_arg1__NSNotificationPtr is `voidptr_t` // so per this page: https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/ObjCRuntimeGuide/Articles/ocrtTypeEncodings.html#//apple_ref/doc/uid/TP40008048-CH100 // and this chat: https://gist.github.com/Noitidart/21b202611c62d74fecd0
+	console.info('rez_class_addMethod:', rez_class_addMethod, rez_class_addMethod.toString(), uneval(rez_class_addMethod));
+	if (rez_class_addMethod != 1) {
+		throw new Error('rez_class_addMethod is not 1, so class_addMethod failed');
+	}
+
+	objc_registerClassPair(NoitNotifCls_class); // returns void // once this line runs ,running objc_allocateClassPair on NoiTNotifCls_class will result it in being NIL
+
+	var NoitNotifCls_instance = objc_msgSend(objc_msgSend(NoitNotifCls_class, alloc), init); // if don't do alloc init on NoitNotifCls_class then the NoitNotifCB_js never triggers
+	console.info('NoitNotifCls_instance:', NoitNotifCls_instance, NoitNotifCls_instance.toString(), uneval(NoitNotifCls_instance), NoitNotifCls_instance.isNull());
+	releaseThese.push(NoitNotifCls_instance);
+	// end - create class and callback
+	
+	// notif_name = [[NSString initWithUTF8String:] 'NSWorkspaceWillLaunchApplicationNotification'];
+	var jsStr_notifName = 'NSWorkspaceWillLaunchApplicationNotification';
 	var NSString = objc_getClass('NSString');
 	var initWithUTF8String = sel_registerName('initWithUTF8String:');
-	notif_name = objc_msgSend(objc_msgSend(NSString, alloc), initWithUTF8String, CHAR.array()(jsStr_notifName));
-	releaseThese.push(notif_name);
-	console.info('notif_name:', notif_name, notif_name.toString(), uneval(notif_name));
-	*/
+	var nsworkspace_will_launch_application_notification = objc_msgSend(objc_msgSend(NSString, alloc), initWithUTF8String, CHAR.array()(jsStr_notifName));
+	console.info('nsworkspace_will_launch_application_notification:', nsworkspace_will_launch_application_notification, nsworkspace_will_launch_application_notification.toString(), uneval(nsworkspace_will_launch_application_notification));
+	releaseThese.push(nsworkspace_will_launch_application_notification);
 	
 	/*
 	// default_center = [NSDistributedNotificationCenter defaultCenter];
@@ -72,6 +92,7 @@ function main() {
 	var default_center = objc_msgSend(NSDistributedNotificationCenter, defaultCenter);
 	console.info('default_center:', default_center, default_center.toString(), uneval(default_center), default_center.isNull());
 	*/
+	
 	// shared_workspace = [NSWorkspace sharedWorkspace];
 	var NSWorkspace = objc_getClass('NSWorkspace');
 	var sharedWorkspace = sel_registerName('sharedWorkspace');
@@ -82,38 +103,7 @@ function main() {
 	var notificationCenter = sel_registerName('notificationCenter');
 	var notification_center = objc_msgSend(shared_workspace, notificationCenter);
 	console.info('notification_center:', notification_center, notification_center.toString(), uneval(notification_center));
-	
-	// nsworkspace_will_launch_application_notification = [[[NSWorkspace sharedWorkspace] notificationCenter] NSWorkspaceWillLaunchApplicationNotification];
-	var NSWorkspaceWillLaunchApplicationNotification = sel_registerName('NSWorkspaceWillLaunchApplicationNotification');
-	var nsworkspace_will_launch_application_notification = objc_msgSend(notification_center, NSWorkspaceWillLaunchApplicationNotification);
-	
-	// start - create class and callback
-	var NSObject = objc_getClass('NSObject');
-	var NoitNotifCls_class = objc_allocateClassPair(NSObject, 'NoitNotifCls', 0);
-	console.info('NoitNotifCls_class:', NoitNotifCls_class, NoitNotifCls_class.toString(), uneval(NoitNotifCls_class), NoitNotifCls_class.isNull());
-	if (NoitNotifCls_class.isNull()) {
-		throw new Error('NoitNotifCls_class is NIL, so objc_allocateClassPair failed');
-	}
-	
-	function NoitNotifCB_js() {
-		console.log('NoitNotifCB_js Triggered');
-	}
-	
-	var NoitNotifCB_imp = IMP_for_notif_cb.ptr(NoitNotifCB_js);
-	
-	var rez_class_addMethod = class_addMethod(NoitNotifCls_class, NoitNotifCB_selector, NoitNotifCB_imp, 'v@:@'); // because return of callback is void, first argument is c_arg1__self which is `id` and c_arg2__id sel `SEL` and objc_arg1__NSNotificationPtr is `voidptr_t` // so per this page: https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/ObjCRuntimeGuide/Articles/ocrtTypeEncodings.html#//apple_ref/doc/uid/TP40008048-CH100 // and this chat: https://gist.github.com/Noitidart/21b202611c62d74fecd0
-	console.info('rez_class_addMethod:', rez_class_addMethod, rez_class_addMethod.toString(), uneval(rez_class_addMethod));
-	if (rez_class_addMethod != 1) {
-		throw new Error('rez_class_addMethod is not 1, so class_addMethod failed');
-	}
 
-	objc_registerClassPair(NoitNotifCls_class);
-
-	var NoitNotifCls_instance = objc_msgSend(objc_msgSend(NoitNotifCls_class, alloc), init);
-	console.info('NoitNotifCls_class_instance:', NoitNotifCls_class_instance, NoitNotifCls_class_instance.toString(), uneval(NoitNotifCls_class_instance), instance__NoitNotifCls_class.isNull());
-	releaseThese.push(NoitNotifCls_instance);
-	// end - create class and callback
-	
 	// addObserver
 	var addObserver = sel_registerName('addObserver:selector:name:object:')
 	objc_msgSend(notification_center, addObserver, NoitNotifCls_instance, NoitNotifCB_selector, nsworkspace_will_launch_application_notification, NIL); // addObserver returns void
@@ -122,12 +112,12 @@ function main() {
 		var removeObserver = sel_registerName('removeObserver:name:object:')
 		objc_msgSend(notification_center, removeObserver, NoitNotifCls_instance, nsworkspace_will_launch_application_notification, NIL); // returns void
 
-		var rez_objc_disposeClassPair = objc_disposeClassPair(NoitNotifCls_class);
-		console.info('rez_objc_disposeClassPair:', rez_objc_disposeClassPair, rez_objc_disposeClassPair.toString(), uneval(rez_objc_disposeClassPair));
 		
 		for (var i=0; i<releaseThese.length; i++) {
 			objc_msgSend(releaseThese[i], release);
 		}
+		
+		objc_disposeClassPair(NoitNotifCls_class); // return is void // must do objc_disposeClassPair AFTER release. if you do release AFTER disposeClasPair then firefox will crash
 		
 		objc.close();
 		
@@ -140,5 +130,5 @@ try {
 } catch (ex) {
 	console.error('Error Occured:', ex);
 } finally {
-	shutdown();
+	//shutdown();
 }

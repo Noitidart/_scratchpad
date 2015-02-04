8e298dcbc5d0620291f23cd8e68e41371f5b1f3d
Noitidart <noitidart@gmail.com>
2015-02-03 22:52:44 -0800

    rev178
    updated Deferred so it throws when not available, and so it doesnt error
    if Promise or PromiseUtils is not imported in the scope

--- deep copy dir.js
+++ deep copy dir.js
@@ -1,46 +1,44 @@
-//start - helper function
-function escapeRegExp(text) {
-	if (!arguments.callee.sRE) {
-		var specials = ['/', '.', '*', '+', '?', '|', '(', ')', '[', ']', '{', '}', '\\'];
-		arguments.callee.sRE = new RegExp('(\\' + specials.join('|\\') + ')', 'g');
-	}
-	return text.replace(arguments.callee.sRE, '\\$1');
-}
-
+//start - helper functions
 function Deferred() {
-	if (Promise.defer) {
+	// this function gets the Deferred object depending on what is available, if not available it throws
+	
+	if (Promise && Promise.defer) {
 		//need import of Promise.jsm for example: Cu.import('resource:/gree/modules/Promise.jsm');
 		return Promise.defer();
-	} else if (PromiseUtils.defer) {
+	} else if (PromiseUtils && PromiseUtils.defer) {
 		//need import of PromiseUtils.jsm for example: Cu.import('resource:/gree/modules/PromiseUtils.jsm');
 		return PromiseUtils.defer();
 	} else {
-		/* A method to resolve the associated Promise with the value passed.
-		 * If the promise is already settled it does nothing.
-		 *
-		 * @param {anything} value : This value is used to resolve the promise
-		 * If the value is a Promise then the associated promise assumes the state
-		 * of Promise passed as value.
-		 */
-		this.resolve = null;
+		try {
+			/* A method to resolve the associated Promise with the value passed.
+			 * If the promise is already settled it does nothing.
+			 *
+			 * @param {anything} value : This value is used to resolve the promise
+			 * If the value is a Promise then the associated promise assumes the state
+			 * of Promise passed as value.
+			 */
+			this.resolve = null;
 
-		/* A method to reject the assocaited Promise with the value passed.
-		 * If the promise is already settled it does nothing.
-		 *
-		 * @param {anything} reason: The reason for the rejection of the Promise.
-		 * Generally its an Error object. If however a Promise is passed, then the Promise
-		 * itself will be the reason for rejection no matter the state of the Promise.
-		 */
-		this.reject = null;
+			/* A method to reject the assocaited Promise with the value passed.
+			 * If the promise is already settled it does nothing.
+			 *
+			 * @param {anything} reason: The reason for the rejection of the Promise.
+			 * Generally its an Error object. If however a Promise is passed, then the Promise
+			 * itself will be the reason for rejection no matter the state of the Promise.
+			 */
+			this.reject = null;
 
-		/* A newly created Pomise object.
-		 * Initially in pending state.
-		 */
-		this.promise = new Promise(function(resolve, reject) {
-			this.resolve = resolve;
-			this.reject = reject;
-		}.bind(this));
-		Object.freeze(this);
+			/* A newly created Pomise object.
+			 * Initially in pending state.
+			 */
+			this.promise = new Promise(function(resolve, reject) {
+				this.resolve = resolve;
+				this.reject = reject;
+			}.bind(this));
+			Object.freeze(this);
+		} catch (ex) {
+			throw new Error('Promise/Deferred is not available');
+		}
 	}
 }
 
@@ -76,7 +74,7 @@ function enumChildEntries(pathToDir, delegate, max_depth, runDelegateOnRoot, dep
 		depth++;
 	}
 	console.log('now, depth:', depth);
-	if ((max_depth === null || max_depth === undefined) || ( /*max_depth !== undefined && max_depth !== null && */ /*this isnt needed as it was tested in the previous if group on this line*/ depth <= max_depth)) {
+	if ((max_depth === null || max_depth === undefined) || ( depth <= max_depth)) {
 		console.log('continue to iterate because either max_depth not exceeded, or max_depth set to go infinite', 'max_depth:', max_depth, 'depth:', depth);
 		//start working here
 		var iterrator = new OS.File.DirectoryIterator(pathToDir);
@@ -157,116 +155,124 @@ function enumChildEntries(pathToDir, delegate, max_depth, runDelegateOnRoot, dep
 	return promise_enumChildEntries;
 }
 
-function duplicateDirAndContents(pathToSrcDir, pathToDestDir, max_depth, targetDirExists) {
-		// returns promise
-		// copies all stuff at depth i, then does depth i + 1, then i + 2 depth, so on // does not start at depth i and if subdir found it doesnt start copying into that right away, it completes depth levels first, i should make this change in future though as enhancement
-		// if targetDirExists mark as true, else, set to false. if you set to true when it does not exist, then promise will reject due to failing to copy to non-existant dir. if it does exist, and you set it to false, then you are just wasting a couple extra function calls, function will complete succesfully though, as it tries to make the dir but it will not overwrite if already found
-
-		var deferred_duplicateDirAndContents = new Deferred();
-		var promise_duplicateDirAndContents = deferred_duplicateDirAndContents.promise;
-
-		var totalEntriesEnummed = 0; //meaning total number of entries ran delegate on, includes root dir if runDelegateOnRoot set to true
-
-		var stuffToMakeAtDepth = [];
-		var smallestDepth = 0;
-		var largestDepth = 0;
-
-		function delegate_handleEntry(entry, depth) {
-			// return true to make enumeration stop
-			if (depth < smallestDepth) {
-				smallestDepth = depth;
-			}
-			if (depth > largestDepth) {
-				largestDepth = depth;
-			}
-			totalEntriesEnummed++;
-			stuffToMakeAtDepth.push({
-				depth: depth,
-				isDir: entry.isDir,
-				path: entry.path
-			});
-		}
-
-		var promise_collectAllPathsInSrcDir = enumChildEntries(pathToSrcDir, delegate_handleEntry, max_depth, !targetDirExists);
-		promise_collectAllPathsInSrcDir.then(
-			function(aVal) {
-				// on resolve, aVal is undefined if it went through all possible entries
-				console.log('Fullfilled - promise_collectAllPathsInSrcDir - ', aVal);
-				console.info('totalEntriesEnummed:', totalEntriesEnummed);
-				console.info('smallestDepth:', smallestDepth);
-				console.info('largestDepth:', largestDepth);
-				console.log('stuffToMakeAtDepth', stuffToMakeAtDepth);
-
-				// start - trigger make promises
-				var curDepth = smallestDepth;
-				var makeStuffsFor_CurDepth = function() {
-					var promiseAllArr_madeForCurDepth = [];
-					for (var i = 0; i < stuffToMakeAtDepth.length; i++) {
-						if (stuffToMakeAtDepth[i].depth == curDepth) {
-							var copyToPath = stuffToMakeAtDepth[i].path.replace(new RegExp(escapeRegExp(pathToTarget), 'i'), pathToDestDir);
-							console.log('copyToPath', copyToPath);
-							promiseAllArr_madeForCurDepth.push(
-								stuffToMakeAtDepth[i].isDir // if (stuffToMakeAtDepth[i].isDir) {
-								?
-									OS.File.makeDir(copyToPath)
-								: // } else {
-									//OS.File.unixSymLink(stuffToMakeAtDepth[i].path, stuffToMakeAtDepth[i].path.replace(new RegExp(escapeRegExp(pathToTarget), 'i'), pathToDestDir))
-									OS.File.copy(stuffToMakeAtDepth[i].path, copyToPath)
-								// }
-							);
-						}
-					}
-					var promiseAll_madeForCurDepth = Promise.all(promiseAllArr_madeForCurDepth);
-					promiseAll_madeForCurDepth.then(
-						function(aVal) {
-							console.log('Fullfilled - promiseAll_madeForCurDepth - ', 'curDepth:', curDepth, aVal);
-							if (curDepth < largestDepth) {
-								curDepth++;
-								makeStuffsFor_CurDepth();
-							} else {
-								deferred_duplicateDirAndContents.resolve('all depths made up to and including:' + largestDepth);
-							}
-						},
-						function(aReason) {
-							var rejObj = {
-								promiseName: 'promiseAll_madeForCurDepth',
-								aReason: aReason,
-								curDepth: curDepth
-							};
-							console.error('Rejected - ' + rejObj.promiseName + ' - ', rejObj);
-							deferred_duplicateDirAndContents.reject(rejObj); // do this instead of throw
-							//throw rejObj; // throw here as its not final // stupid of me, just dont throw so it rejects gracefully
-						}
-					).catch(
-						function(aCaught) {
-							console.error('Caught - promiseAll_madeForCurDepth - ', aCaught);
-							throw aCaught; //throw here as its not final catch
-						}
-					);
-				};
-				makeStuffsFor_CurDepth(); //start the making loop
-				// end - trigger make promises
-				//return here is useless, we need to deal with `deferred_duplicateDirAndContents` //return promise_allStuffsMadeForCollectedPaths; //this is the final then, we leave that to the dev who is using this helper function, so i dont return .then, i return the promise, the dev handles the .then, so commented out following block:
-			},
-			function(aReason) {
-				var rejObj = {
-					promiseName: 'promise_collectAllPathsInSrcDir',
-					aReason: aReason
-				};
-				console.error('Rejected - ' + rejObj.promiseName + ' - ', rejObj);
-				deferred_duplicateDirAndContents.reject(rejObj); // do this instead of throw
-				//throw rejObj; // man i always was throwing on reject, this was stupid of me, this is not PromiseWorker, i should only throw when a real error occurs, otherwise it will go into the catch of the final statement instead of the reject
-			}
-		).catch(
-			function(aCatch) {
-				console.error('Caught - promise_collectAllPathsInSrcDir - ', aCatch);
-				throw aCatch;
-			}
-		);
-
-		return promise_duplicateDirAndContents;
+function escapeRegExp(text) {
+	if (!arguments.callee.sRE) {
+		var specials = ['/', '.', '*', '+', '?', '|', '(', ')', '[', ']', '{', '}', '\\'];
+		arguments.callee.sRE = new RegExp('(\\' + specials.join('|\\') + ')', 'g');
 	}
-	// end - helper function
+	return text.replace(arguments.callee.sRE, '\\$1');
+}
+
+function duplicateDirAndContents(pathToSrcDir, pathToDestDir, max_depth, targetDirExists) {
+	// returns promise
+	// copies all stuff at depth i, then does depth i + 1, then i + 2 depth, so on // does not start at depth i and if subdir found it doesnt start copying into that right away, it completes depth levels first, i should make this change in future though as enhancement
+	// if targetDirExists mark as true, else, set to false. if you set to true when it does not exist, then promise will reject due to failing to copy to non-existant dir. if it does exist, and you set it to false, then you are just wasting a couple extra function calls, function will complete succesfully though, as it tries to make the dir but it will not overwrite if already found
+
+	var deferred_duplicateDirAndContents = new Deferred();
+	var promise_duplicateDirAndContents = deferred_duplicateDirAndContents.promise;
+
+	var totalEntriesEnummed = 0; //meaning total number of entries ran delegate on, includes root dir if runDelegateOnRoot set to true
+
+	var stuffToMakeAtDepth = [];
+	var smallestDepth = 0;
+	var largestDepth = 0;
+
+	var delegate_handleEntry = function(entry, depth) {
+		// return true to make enumeration stop
+		if (depth < smallestDepth) {
+			smallestDepth = depth;
+		}
+		if (depth > largestDepth) {
+			largestDepth = depth;
+		}
+		totalEntriesEnummed++;
+		stuffToMakeAtDepth.push({
+			depth: depth,
+			isDir: entry.isDir,
+			path: entry.path
+		});
+	};
+
+	var promise_collectAllPathsInSrcDir = enumChildEntries(pathToSrcDir, delegate_handleEntry, max_depth, !targetDirExists);
+	promise_collectAllPathsInSrcDir.then(
+		function(aVal) {
+			// on resolve, aVal is undefined if it went through all possible entries
+			console.log('Fullfilled - promise_collectAllPathsInSrcDir - ', aVal);
+			console.info('totalEntriesEnummed:', totalEntriesEnummed);
+			console.info('smallestDepth:', smallestDepth);
+			console.info('largestDepth:', largestDepth);
+			console.log('stuffToMakeAtDepth', stuffToMakeAtDepth);
+
+			// start - trigger make promises
+			var curDepth = smallestDepth;
+			var makeStuffsFor_CurDepth = function() {
+				var promiseAllArr_madeForCurDepth = [];
+				for (var i = 0; i < stuffToMakeAtDepth.length; i++) {
+					if (stuffToMakeAtDepth[i].depth == curDepth) {
+						var copyToPath = stuffToMakeAtDepth[i].path.replace(new RegExp(escapeRegExp(pathToTarget), 'i'), pathToDestDir);
+						console.log('copyToPath', copyToPath);
+						promiseAllArr_madeForCurDepth.push(
+							stuffToMakeAtDepth[i].isDir // if (stuffToMakeAtDepth[i].isDir) {
+							?
+								OS.File.makeDir(copyToPath)
+							: // } else {
+								//OS.File.unixSymLink(stuffToMakeAtDepth[i].path, stuffToMakeAtDepth[i].path.replace(new RegExp(escapeRegExp(pathToTarget), 'i'), pathToDestDir))
+								OS.File.copy(stuffToMakeAtDepth[i].path, copyToPath)
+							// }
+						);
+					}
+				}
+				var promiseAll_madeForCurDepth = Promise.all(promiseAllArr_madeForCurDepth);
+				promiseAll_madeForCurDepth.then(
+					function(aVal) {
+						console.log('Fullfilled - promiseAll_madeForCurDepth - ', 'curDepth:', curDepth, aVal);
+						if (curDepth < largestDepth) {
+							curDepth++;
+							makeStuffsFor_CurDepth();
+						} else {
+							deferred_duplicateDirAndContents.resolve('all depths made up to and including:' + largestDepth);
+						}
+					},
+					function(aReason) {
+						var rejObj = {
+							promiseName: 'promiseAll_madeForCurDepth',
+							aReason: aReason,
+							curDepth: curDepth
+						};
+						console.error('Rejected - ' + rejObj.promiseName + ' - ', rejObj);
+						deferred_duplicateDirAndContents.reject(rejObj); // do this instead of throw
+						//throw rejObj; // throw here as its not final // stupid of me, just dont throw so it rejects gracefully
+					}
+				).catch(
+					function(aCaught) {
+						console.error('Caught - promiseAll_madeForCurDepth - ', aCaught);
+						throw aCaught; //throw here as its not final catch
+					}
+				);
+			};
+			makeStuffsFor_CurDepth(); //start the making loop
+			// end - trigger make promises
+			//return here is useless, we need to deal with `deferred_duplicateDirAndContents` //return promise_allStuffsMadeForCollectedPaths; //this is the final then, we leave that to the dev who is using this helper function, so i dont return .then, i return the promise, the dev handles the .then, so commented out following block:
+		},
+		function(aReason) {
+			var rejObj = {
+				promiseName: 'promise_collectAllPathsInSrcDir',
+				aReason: aReason
+			};
+			console.error('Rejected - ' + rejObj.promiseName + ' - ', rejObj);
+			deferred_duplicateDirAndContents.reject(rejObj); // do this instead of throw
+			//throw rejObj; // man i always was throwing on reject, this was stupid of me, this is not PromiseWorker, i should only throw when a real error occurs, otherwise it will go into the catch of the final statement instead of the reject
+		}
+	).catch(
+		function(aCatch) {
+			console.error('Caught - promise_collectAllPathsInSrcDir - ', aCatch);
+			throw aCatch;
+		}
+	);
+
+	return promise_duplicateDirAndContents;
+}
+// end - helper functions
 
 var pathToTarget = OS.Path.join(OS.Constants.Path.desktopDir, 'trgt folder');
 var pathToCreate = OS.Path.join(OS.Constants.Path.desktopDir, 'deep copied dir'); // does not have to exist, but if it doesnt, then make sure to pass last argument of duplicateDirAndContents of `targetDirExists` as false or null or undefined. dont pass true, otherwise you lied to it and it will reject as the destintation dir doesnt exist

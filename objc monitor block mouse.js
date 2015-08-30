45f952678d4183f30e1953f0e74dbfd3ea37419b
Noitidart <noitidart@gmail.com>
2015-08-30 01:00:07 -0700

    code is crashing

--- objc monitor block mouse.js
+++ objc monitor block mouse.js
@@ -5,7 +5,8 @@ var is64bit = ctypes.voidptr_t.size == 4 ? false : true;
 
 // BASIC TYPES
 var TYPES = {
-	id: ctypes.voidptr_t,
+	char: ctypes.char,
+	ID: ctypes.voidptr_t,
 	IMP: ctypes.voidptr_t,
 	SEL: ctypes.voidptr_t,
 	Class: ctypes.voidptr_t,
@@ -60,43 +61,43 @@ var CONST = {
 // the NSEventMask stuff is wrong in docs, the docs say here: https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSEvent_Class/index.html#//apple_ref/doc/constant_group/NSEventMaskFromType
 	// the actual source code says here: /System/Library/Frameworks/AppKit.framework/Versions/C/Headers/NSEvent.h   pasted the part here thanks to @arai - https://gist.github.com/Noitidart/9470ec02bd252e2ae7eb
 	// see chat with @arai and @capella on Aug 29 2015
-CONST.NSLeftMouseDownMask = 1 << CONST.NSLeftMouseDown,
-CONST.NSLeftMouseUpMask = 1 << CONST.NSLeftMouseUp,
-CONST.NSRightMouseDownMask = 1 << CONST.NSRightMouseDown,
-CONST.NSRightMouseUpMask = 1 << CONST.NSRightMouseUp,
-CONST.NSMouseMovedMask = 1 << CONST.NSMouseMoved,
-CONST.NSLeftMouseDraggedMask = 1 << CONST.NSLeftMouseDragged,
-CONST.NSRightMouseDraggedMask = 1 << CONST.NSRightMouseDragged,
-CONST.NSMouseEnteredMask = 1 << CONST.NSMouseEntered,
-CONST.NSMouseExitedMask = 1 << CONST.NSMouseExited,
-CONST.NSKeyDownMask = 1 << CONST.NSKeyDown,
-CONST.NSKeyUpMask = 1 << CONST.NSKeyUp,
-CONST.NSFlagsChangedMask = 1 << CONST.NSFlagsChanged,
-CONST.NSAppKitDefinedMask = 1 << CONST.NSAppKitDefined,
-CONST.NSSystemDefinedMask = 1 << CONST.NSSystemDefined,
-CONST.NSApplicationDefinedMask = 1 << CONST.NSApplicationDefined,
-CONST.NSPeriodicMask = 1 << CONST.NSPeriodic,
-CONST.NSCursorUpdateMask = 1 << CONST.NSCursorUpdate,
-CONST.NSScrollWheelMask = 1 << CONST.NSScrollWheel,
-CONST.NSTabletPointMask = 1 << CONST.NSTabletPoint,
-CONST.NSTabletProximityMask = 1 << CONST.NSTabletProximity,
-CONST.NSOtherMouseDownMask = 1 << CONST.NSOtherMouseDown,
-CONST.NSOtherMouseUpMask = 1 << CONST.NSOtherMouseUp,
-CONST.NSOtherMouseDraggedMask = 1 << CONST.NSOtherMouseDragged,
-CONST.NSEventMaskGesture = 1 << CONST.NSEventTypeGesture,
-CONST.NSEventMaskMagnify = 1 << CONST.NSEventTypeMagnify,
-CONST.NSEventMaskSwipe = 1 << CONST.NSEventTypeSwipe,	// 1U << NSEventTypeSwipe
-CONST.NSEventMaskRotate = 1 << CONST.NSEventTypeRotate,
-CONST.NSEventMaskBeginGesture = 1 << CONST.NSEventTypeBeginGesture,
-CONST.NSEventMaskEndGesture = 1 << CONST.NSEventTypeEndGesture,
-CONST.NSEventMaskSmartMagnify = 1 << CONST.NSEventTypeSmartMagnify,	// 1ULL << NSEventTypeSmartMagnify,
-CONST.NSEventMaskPressure = 1 << CONST.NSEventTypePressure,	// 1ULL << NSEventTypePressure
-CONST.NSAnyEventMask = CONST.NSUIntegerMax //0xffffffffU
+CONST.NSLeftMouseDownMask = 1 << CONST.NSLeftMouseDown;
+CONST.NSLeftMouseUpMask = 1 << CONST.NSLeftMouseUp;
+CONST.NSRightMouseDownMask = 1 << CONST.NSRightMouseDown;
+CONST.NSRightMouseUpMask = 1 << CONST.NSRightMouseUp;
+CONST.NSMouseMovedMask = 1 << CONST.NSMouseMoved;
+CONST.NSLeftMouseDraggedMask = 1 << CONST.NSLeftMouseDragged;
+CONST.NSRightMouseDraggedMask = 1 << CONST.NSRightMouseDragged;
+CONST.NSMouseEnteredMask = 1 << CONST.NSMouseEntered;
+CONST.NSMouseExitedMask = 1 << CONST.NSMouseExited;
+CONST.NSKeyDownMask = 1 << CONST.NSKeyDown;
+CONST.NSKeyUpMask = 1 << CONST.NSKeyUp;
+CONST.NSFlagsChangedMask = 1 << CONST.NSFlagsChanged;
+CONST.NSAppKitDefinedMask = 1 << CONST.NSAppKitDefined;
+CONST.NSSystemDefinedMask = 1 << CONST.NSSystemDefined;
+CONST.NSApplicationDefinedMask = 1 << CONST.NSApplicationDefined;
+CONST.NSPeriodicMask = 1 << CONST.NSPeriodic;
+CONST.NSCursorUpdateMask = 1 << CONST.NSCursorUpdate;
+CONST.NSScrollWheelMask = 1 << CONST.NSScrollWheel;
+CONST.NSTabletPointMask = 1 << CONST.NSTabletPoint;
+CONST.NSTabletProximityMask = 1 << CONST.NSTabletProximity;
+CONST.NSOtherMouseDownMask = 1 << CONST.NSOtherMouseDown;
+CONST.NSOtherMouseUpMask = 1 << CONST.NSOtherMouseUp;
+CONST.NSOtherMouseDraggedMask = 1 << CONST.NSOtherMouseDragged;
+CONST.NSEventMaskGesture = 1 << CONST.NSEventTypeGesture;
+CONST.NSEventMaskMagnify = 1 << CONST.NSEventTypeMagnify;
+CONST.NSEventMaskSwipe = 1 << CONST.NSEventTypeSwipe;	// 1U << NSEventTypeSwipe
+CONST.NSEventMaskRotate = 1 << CONST.NSEventTypeRotate;
+CONST.NSEventMaskBeginGesture = 1 << CONST.NSEventTypeBeginGesture;
+CONST.NSEventMaskEndGesture = 1 << CONST.NSEventTypeEndGesture;
+CONST.NSEventMaskSmartMagnify = 1 << CONST.NSEventTypeSmartMagnify;	// 1ULL << NSEventTypeSmartMagnify;
+CONST.NSEventMaskPressure = 1 << CONST.NSEventTypePressure;	// 1ULL << NSEventTypePressure
+CONST.NSAnyEventMask = CONST.NSUIntegerMax; //0xffffffffU
 
 // COMMON FUNCTIONS
-var objc_getClass = objc.declare('objc_getClass', ctypes.default_abi, ID, CHAR.ptr);
-var objc_msgSend = objc.declare('objc_msgSend', ctypes.default_abi, ID, ID, SEL, '...');
-var sel_registerName = objc.declare('sel_registerName', ctypes.default_abi, SEL, CHAR.ptr);
+var objc_getClass = objc.declare('objc_getClass', ctypes.default_abi, TYPES.ID, TYPES.char.ptr);
+var objc_msgSend = objc.declare('objc_msgSend', ctypes.default_abi, TYPES.ID, TYPES.ID, TYPES.SEL, '...');
+var sel_registerName = objc.declare('sel_registerName', ctypes.default_abi, TYPES.SEL, TYPES.char.ptr);
 
 // COMMON SELECTORS
 var release = sel_registerName('release');
@@ -112,13 +113,23 @@ function shutdown() {
 	objc.close();
 };
 
+var myHandler_js;
+var myHandler_c;
 function main() {
 
 	// current_application = [NSRunningApplication currentApplciation];
 	var NSEvent = objc_getClass('NSEvent');
 	var addLocalMonitorForEventsMatchingMask = sel_registerName('addLocalMonitorForEventsMatchingMask:handler:');
 
-	// var bundle_url = objc_msgSend(NSEvent, addLocalMonitorForEventsMatchingMask);
+	myHandler_js = function(c_arg1__self, c_arg2__sel) {
+		console.log('in myHandler', c_arg2__sel.toString());
+		return TYPES.ID(0);
+	};
+	var IMP_for_imageNamed = ctypes.FunctionType(ctypes.default_abi, TYPES.ID, [TYPES.ID, TYPES.ID]);
+	myHandler_c = IMP_for_imageNamed.ptr(myHandler_js);
+	
+	var rez_add = objc_msgSend(NSEvent, addLocalMonitorForEventsMatchingMask, TYPES.NSEventMask(CONST.NSKeyDownMask), myHandler_c);
+	console.log('rez_add:', rez_add, rez_add.toString());
 }
 
 try {
